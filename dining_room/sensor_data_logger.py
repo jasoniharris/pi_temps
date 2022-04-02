0720830fa8f51cd8d827717bc16ed6df75a5c547
Jason <jiharris90@gmail.com>
2022-04-02 16:42:30 +0100

    Refactoring structure for dynamic content

---
+++ dining_room/sensor_data_logger.py
@@ -0,0 +1,60 @@
+import time
+import sys
+import datetime
+import board
+import adafruit_dht
+from influxdb import InfluxDBClient
+import requests
+import json
+
+with open('config.json') as config_file:
+    data = json.load(config_file)
+
+measurement = data['measurement']
+location = data['location']
+host = data['host']
+port = data['port']
+user = data['user']
+password = data['password']
+dbname = data['dbname']
+interval = data['interval']
+
+client = InfluxDBClient(host, port, user, password, dbname)
+
+dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+
+while True:
+    try:
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        iso = time.ctime()
+        # Print for debugging, uncomment the below line
+        print("[%s] Temp: %s, Humidity: %s" % (iso, temperature_c, humidity))
+
+        data = [
+        {
+          "measurement": measurement,
+              "tags": {
+                  "location": location,
+              },
+              "fields": {
+                  "temperature" : temperature_c,
+                  "humidity": humidity,
+              }
+          }
+        ]
+
+        # Send the JSON data to InfluxDB
+        client.write_points(data)
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        # print(error.args[0])
+        time.sleep(10.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    time.sleep(interval)
---
+++ outside/outside_temp_logger.py
@@ -0,0 +1,60 @@
+import time
+import sys
+import datetime
+import requests
+from influxdb import InfluxDBClient
+import json
+
+with open('config.json') as config_file:
+    data = json.load(config_file)
+
+appid = data['appid']
+measurement = data['measurement']
+location = data['location']
+host = data['host']
+port = data['port']
+user = data['user']
+password = data['password']
+dbname = data['dbname']
+interval = data['interval']
+
+# Create the InfluxDB client object
+client = InfluxDBClient(host, port, user, password, dbname)
+
+params = {"lat": "52.840056", "lon": "-2.471194", "units": "metric", "appid": appid}
+baseurl = "https://api.openweathermap.org/data/2.5/weather"
+content_type = str('text/plain; version=0.0.4; charset=utf-8')
+
+def get_outside_weather():
+    response = requests.get(baseurl, params=params)
+    temp = response.json()['main']['temp']
+    return temp
+
+while True:
+    try:
+        outside_temp = get_outside_weather()
+        iso = time.ctime()
+        print("[%s] Outside: %s" % (iso, outside_temp))
+
+        data = [
+        {
+          "measurement": measurement,
+              "tags": {
+                  "location": location,
+              },
+              "fields": {
+                  "temperature" : outside_temp
+              }
+          }
+        ]
+        # Send the JSON data to InfluxDB
+        client.write_points(data)
+
+    except RuntimeError as error:
+        # print(error.args[0])
+        time.sleep(10.0)
+        continue
+    except Exception as error:
+        raise error
+
+    time.sleep(interval)
