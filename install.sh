abdb4920448fcffa2236dd920b8944bffa965f08
Jason Harris <harrzjas@amazon.com>
2023-07-04 14:55:52 +0100

    Updating room name

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp dining_room/*.service /etc/systemd/system/
+sudo cp inside/*.service /etc/systemd/system/
 sudo cp outside/*.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
@@ -8,5 +8,4 @@ sudo systemctl daemon-reload
 systemctl restart sensor_data_logger
 systemctl restart outside_temp_logger
 
-if [ -z ${pi_temps_appid+x} ]; then echo "appid is unset"; else echo "appid is set to '${pi_temps_appid}'"; fi
 
