e965717783106514f8697011c2b0176db9f87e0b
Jason Harris <harrzjas@amazon.com>
2023-07-05 18:47:37 +0100

    CHanging to crontab for publisher

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 sudo cp inside/*.service /etc/systemd/system/
 sudo cp outside/*.service /etc/systemd/system/
-sudo cp alert_publisher/*.service /etc/systemd/system/
 
 aws s3 cp s3://harris-pi-temps/outside_config.json outside/ --profile pi
 aws s3 cp s3://harris-pi-temps/inside_config.json inside/ --profile pi
@@ -11,6 +10,5 @@ sudo systemctl daemon-reload
 
 systemctl restart sensor_data_logger
 systemctl restart outside_temp_logger
-systemctl restart alert_publisher
 
 
